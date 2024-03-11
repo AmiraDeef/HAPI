cc5eac7af204e45c052de8c1e79cc6832664b2a9
AmiraDeef <amiradeef4@gmail.com>
2024-03-12 01:18:22 +0200

    "modifying with IOT"

--- app/Http/Controllers/Crop/RecommendationController.php
+++ app/Http/Controllers/Crop/RecommendationController.php
@@ -26,27 +26,36 @@ class RecommendationController extends Controller
         $land = Auth::user()->landowner->land;
 
         try {
-            $response = Http::post('https://e376e3b7-2a57-4420-9342-3717ad9cec0a.mock.pstmn.io/land-info', [
-                'land_id' => $land->unique_land_id,
-            ]);
+//            $response = Http::post('https://e376e3b7-2a57-4420-9342-3717ad9cec0a.mock.pstmn.io/land-info', [
+//                'land_id' => $land->unique_land_id,
+//            ]);
+            $iot_data = Iot::where('land_id', $land->id)->latest()->first();
 
-            if ($response->successful()) {
-                $landInfo = $response->json();
-                $cropRecommendations = $this->getCropRecommendation($landInfo);
-                $this->sendCropRecommendations($cropRecommendations);
-            } else {
-                //just for test ,I'll refactor it later..
-                switch ($response->status()) {
-                    case 400:
-                        return response()->json(['error' => 'Bad request'], 400);
-                    case 404:
-                        return response()->json(['error' => 'Land information not found'], 404);
-                    case 500:
-                        return response()->json(['error' => 'Server error'], 500);
-                    default:
-                        return response()->json(['error' => 'Failed to get land information'], $response->status());
-                }
+            if (!$iot_data) {
+                return response()->json(['error' => 'No IoT data found for the specified land'], 404);
             }
+
+//            if ($response->successful()) {
+//                $landInfo = $response->json();
+                $cropRecommendations = $this->getCropRecommendation($iot_data->data);
+            return response()->json([
+                'message' => 'Crop recommendations fetched successfully',
+                'crop_recommendations' => $cropRecommendations,
+            ], 200);
+
+//            }else {
+                //just for test ,I'll refactor it later..
+//                switch ($response->status()) {
+//                    case 400:
+//                        return response()->json(['error' => 'Bad request'], 400);
+//                    case 404:
+//                        return response()->json(['error' => 'Land information not found'], 404);
+//                    case 500:
+//                        return response()->json(['error' => 'Server error'], 500);
+//                    default:
+//                        return response()->json(['error' => 'Failed to get land information'], $response->status());
+//                }
+//            }
         } catch (\Exception $e) {
             Log::error('Error processing HTTP request: ' . $e->getMessage());
             Log::error($e->getTraceAsString());
@@ -54,6 +63,7 @@ class RecommendationController extends Controller
         }
 
     }
+
     private function getCropRecommendation($landInfo)
     {
 //get the crop recommendation
@@ -64,13 +74,13 @@ class RecommendationController extends Controller
         return $response->json();
     }
 
-    private function sendCropRecommendations($cropRecommendations)
-    {
-// Send the response with crop recommendations and success message
-        return response()->json([
-            'message' => 'Land information received successfully',
-            'crop_recommendations' => $cropRecommendations,
-        ], 200);
-    }
+//    private function sendCropRecommendations($cropRecommendations)
+//    {
+//// Send the response with crop recommendations and success message
+//        return response()->json([
+//            'message' => 'Land information received successfully',
+//            'crop_recommendations' => $cropRecommendations,
+//        ], 200);
+//    }
 
 }
