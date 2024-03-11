c80556196080a530677c707799c52d3e7e93f566
AmiraDeef <amiradeef4@gmail.com>
2024-03-12 01:15:50 +0200

    "create Iot Model"

---
+++ app/Models/Iot.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Iot extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'land_id','data','created_at','updated_at'
+    ];
+    public function land(){
+        return $this->belongsTo(Land::class);
+    }
+
+}
