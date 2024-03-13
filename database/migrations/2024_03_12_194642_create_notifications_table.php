ea4f4fe6981a391eb0f1bc7b2b16493fc4a3c3e7
AmiraDeef <amiradeef4@gmail.com>
2024-03-14 00:21:02 +0200

    add type message

--- database/migrations/2024_03_12_194642_create_notifications_table.php
+++ database/migrations/2024_03_12_194642_create_notifications_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('land_id');
             $table->enum('status',['read','unread'])->default('unread');
-            $table->enum('type',['new_farmer','new_iot_actions','new_detection']);
+            $table->enum('type',['new_farmer','new_iot_actions','new_detection','message']);
             $table->string('message');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('land_id')->references('unique_land_id')->on('lands')->onDelete('cascade');
