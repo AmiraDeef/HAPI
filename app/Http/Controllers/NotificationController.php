6670d185f59e6af90f3b554e5780f1af40e61197
AmiraDeef <amiradeef4@gmail.com>
2024-03-13 03:54:44 +0200

    "refactoring newFarmerNotificaton "

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -31,7 +31,48 @@ class NotificationController extends Controller
         return response()->json(['notifications' => $notifications], 200);
 
     }
-    private function createNewFarmerNotification($land_id,$username){
+
+
+    public function createNewFarmerNotification($land_id, $username)
+    {
+        try {
+            // Attempt to find the land with the provided ID
+            $land = Land::where('unique_land_id', $land_id)->with('landowner')->firstOrFail();
+            $landowner = $land->landowner;
+
+            if ($landowner) {
+                Notification::create([
+                    'land_id' => $land_id,
+                    'user_id' => $landowner->user->id,
+                    'message' => $username . ' has requested to access your land',
+                    'type' => 'new_farmer',
+                    'status' => 'unread',
+                ]);
+            }
+
+            // Success message (optional)
+            // return response()->json(['message' => 'Notifications created successfully']);
+
+        } catch (ModelNotFoundException $e) {
+            // Handle case where land is not found
+            return response()->json(['error' => 'Land with ID ' . $land_id . ' not found'], 404);
+        }
+    }
+
+    public function createNewDetectionNotification($land_id,$username){
+        $users = Land::findOrFail($land_id)->users;
+        foreach ($users as $user) {
+            Notification::create([
+                'land_id' => $land_id,
+                'user_id' => $user->id,
+                'message' => $username . ' has recorded a new detection',
+                'type' => 'new_detection',
+                'status' => 'unread',
+            ]);
+        }
+
+    }
+    public function createNewIotNotification($land_id){
         $landowners = Land::findOrFail($land_id)->landowners;
            foreach ($landowners as $landowner) {
                Notification::create([
