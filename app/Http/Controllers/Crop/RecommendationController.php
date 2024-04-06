640738ef490bcb68b57469a585c1f5a54b11c863
AmiraDeef <amiradeef4@gmail.com>
2024-04-06 17:33:40 +0200

    refactor recommendation controller

--- app/Http/Controllers/Crop/RecommendationController.php
+++ app/Http/Controllers/Crop/RecommendationController.php
@@ -25,26 +25,20 @@ class RecommendationController extends Controller
             return response()->json(['error' => 'Unauthorized'], 401);
         }
         $land = Auth::user()->landowner->lands()->first();
-
+        $iot_data = Iot::where('land_id', $land->id)->latest()->first();
+        if (!$iot_data) {
+            return response()->json(['error' => 'No IoT data found for the specified land'], 404);
+        }
         try {
 //            $response = Http::post('https://e376e3b7-2a57-4420-9342-3717ad9cec0a.mock.pstmn.io/land-info', [
 //                'land_id' => $land->unique_land_id,
 //            ]);
-            $iot_data = Iot::where('land_id', $land->id)->latest()->first();
-
-            if (!$iot_data) {
-                return response()->json(['error' => 'No IoT data found for the specified land'], 404);
-            }
-
-//            if ($response->successful()) {
-//                $landInfo = $response->json();
-                $cropRecommendations = $this->getCropRecommendation($iot_data->data);
-            return response()->json([
-                'message' => 'Crop recommendations fetched successfully',
-                'crop_recommendations' => $cropRecommendations,
-            ], 200);
+            $cropRecommendations = $this->getCropRecommendation($iot_data->data);
+            // Return the crop recommendations
+            return response()->json($cropRecommendations, 200);
 
         } catch (\Exception $e) {
+
             Log::error('Error processing HTTP request: ' . $e->getMessage());
             Log::error($e->getTraceAsString());
             return response()->json(['error' => 'Failed to process HTTP request'], 500);
@@ -56,29 +50,17 @@ class RecommendationController extends Controller
     {
         // Convert JSON string to associative array
         $data = json_decode($landInfo, true);
-
-        // Check if decoding was successful
         if ($data === null) {
             return response()->json(['error' => 'Invalid land information format.'], 400);
         }
 
-        // Make HTTP POST request to the Flask API endpoint
         $response = Http::post("http://127.0.0.1:5000/get-crop-recommendation", $data);
 
-        // Check if the request was successful
         if (!$response->successful()) {
             return response()->json(['error' => 'Failed to get crop recommendation.'], $response->status());
         }
+
         return $response->json();
     }
-
-//    private function sendCropRecommendations($cropRecommendations)
-//    {
-//// Send the response with crop recommendations and success message
-//        return response()->json([
-//            'message' => 'Land information received successfully',
-//            'crop_recommendations' => $cropRecommendations,
-//        ], 200);
-//    }
 
 }
